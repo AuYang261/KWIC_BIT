22c784602ce2b8f4608de663e82cd60ac167f6b4
lankerr <82802866+lankerr@users.noreply.github.com>
2022-03-27 17:20:59 +0800

    Add files via upload

--- passage类，row类，（待修改）.cpp
+++ passage类，row类，（待修改）.cpp
@@ -1,65 +1,88 @@
+#include<vector>
+#include<iostream>
+#include<list>
+#include<string>
+#include<stdlib.h>
+#include<algorithm>
+using namespace std;
+class Word{
+public://must be a public or it be private 
+string	content;
+bool flagFind;
+int id;
+};
+class Text{
+	public:
+vector<Word>words;
+};
+class Row{
+vector<Word>::iterator words;
+Text textPrototype;
+};
 class Passage{
 public:
 list<Text> texts;
-vector<row> rows; 
+vector<Row> rows;
 sortRow(){
-int n=ls.size();
+sort(rows.begin(),rows.end(),[=](Row a,Row b)->{return(a<b);});
+}
+void input(FILE *fp){//此处用的是文件流,返回给自己
+if((fp=fopen(filename,"r"))==NULL){throw ("ERROR");}
+string line;
+else while(getline(fp,line)){//这里这样子不知道能不能运行qwq
+string tmp;
+Text t;
+tmp=strok(line," ");//strok 按照空格分开
+int i=0;
+while(tmp){
+    Word temp;
+    temp.content=tmp;
+    temp.id=i;
+    temp.flagFind=0;
+    t.words.push_back(temp);
+    i++; tmp=strok(NULL," ");
+}
+texts.push_back(t);
+}
+}
+string toStr(){
+string tmp;
+string temp;
+int n=(signed)rows.size();
 for(int i=0;i<n;i++){
-    for(int j=0;j<i;j++){
-if(ls[j]<ls[i]){
-Row s=ls[j];
-ls[j]=ls[i];
-ls[i]=s;
+tmp=rows[i].toStr();
+temp+=tmp;
+temp+='\n';
 }
+return temp;
 }
-}
-}
-input(Text){
-    vector<int>temp;
-    string n,m;
-    p=text;
-    while(p->next!=NULL){//这里假定p是空头结点
-        p=p->next;
-        list=p->data;//这里对p的数据域暂定为data
-        do{scanf("%s",m);m.puh_back(temp)}while(scanf("%s",&n)!=EOF);
-    }
-}
-toStr(vector<Row>ls){
-vector<string>tmp;
-for(int i=0;i<ls.size();i++){
-Row temp=ls[i];
-tmp.push_back(temp);
-}
-tmp=toStr(tmp);
-return tmp;
-}
-}
+};
 class Row{
-list order;
+public:
+vector<Word>::iterator pos;
 Text textPrototype;
-void operator<(Row a,Row b){
+bool operator < (const Row& a)const{
 int num(char s){
 int n=int(s);
 if('A'<=s&&s<='Z'){return 2*(n-64)}
 else if('a'<=s&&s<='z'){return 2*(n-97)-1}
 else{return 0};
 }
-str1=toStr(a);str2=toStr(b);
+str1=this.toStr();str2=a.toStr();
 int A=0,B=0;
 for(int i=0,i<string.length();i++){
     A+=num(str1[i]);B+=num(str2[i]);
     A*=100;B*=100;
 }//构造了一个可以比较大小的转化方法
 if(A<B){return 1};
-else if (A==B){RaiseException(they are of the same size!);}
+else if (A==B){throw(ERROR!);}
 else return 0;
 }
-string toStr(Row a){
-/*return std::string str(a.textPrototype.words.begin(),a.textPrototype.words.end());*/
-vector<word>list=a.textPrototype.words;
-vector<string>temp;
-for(int i=0;i<list.size();i++){
-    temp.push_back(list.words[order[i]].comtent);
-}//按照order，嵌套很多，不知道对不对qwq
-return std::string str(temp.begin(),temp.end());
+string toStr(){
+string temp;
+vector<Word>interator i;
+for(i=pos.begin();i!=pos.end();i++){
+    temp+=i.content;
 }
+return temp;
+};}
