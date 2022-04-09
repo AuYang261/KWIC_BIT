d52af18f304fe18837b67439c96908497ccd222c
unknown <459461160@qq.com>
2022-04-09 22:57:52 +0800

    删除Sortor多余代码

--- src_cpp/Sortor.cpp
+++ src_cpp/Sortor.cpp
@@ -1,39 +1,5 @@
 #include "Sortor.h"
 
-#include <iostream>
-#include <vector>
-void swap(Row *&a, Row *&b) {
-    auto temp = a;
-    a = b;
-    b = temp;
-}
-void adjust(std::vector<Row *> &arr, int len, int index) {
-    int left = 2 * index + 1;   // index的左子节点
-    int right = 2 * index + 2;  // index的右子节点
-
-    int maxIdx = index;
-    if (left < len && arr[maxIdx] < arr[left]) maxIdx = left;
-    if (right < len && arr[maxIdx] < arr[right]) maxIdx = right;
-
-    if (maxIdx != index) {
-        swap(arr[maxIdx], arr[index]);
-        adjust(arr, len, maxIdx);
-    }
-}
-
-// 堆排序
-void heapSort(std::vector<Row *> &arr, int size) {
-    // 构建大根堆（从最后一个非叶子节点向上）
-    for (int i = size / 2 - 1; i >= 0; i--) {
-        adjust(arr, size, i);
-    }
-
-    // 调整大根堆
-    for (int i = size - 1; i >= 1; i--) {
-        swap(arr[0], arr[i]);  // 将当前最大的放置到数组末尾
-        adjust(arr, i, 0);  // 将未完成排序的部分继续进行堆排序
-    }
-}
 void Sortor1::sort(std::list<Row *> &rows) {
     //降序，*a<*b则为升序
     rows.sort([](Row *a, Row *b) -> bool { return (*b < *a); });
@@ -47,7 +13,6 @@ void Sortor2::sort(std::list<Row *> &rows) {
     }
     //降序，*a<*b则为升序
     std::sort(v.begin(), v.end(), [](Row *a, Row *b) { return *b < *a; });
-    // heapSort(v, v.size());
     rows.clear();
     for (auto i : v) {
         rows.push_back(i);
