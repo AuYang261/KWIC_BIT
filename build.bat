85b1cc4f55e6878707e2c5062b61d4992111ca7a
unknown <459461160@qq.com>
2022-04-11 19:10:00 +0800

    include分离成功编译

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
 set pythonPath=C:\Users\AuYang\AppData\Local\Programs\Python\Python36\
-g++ src_cpp/* -I%pythonPath%include -L%pythonPath%libs -lpython36 -L%pythonPath% -lpython36 -o GUI/PyAPI.pyd -shared -std=c++20 -static > BuildOutput.log 2>&1
+g++ src_cpp/*.cpp -Isrc_cpp/include -I%pythonPath%include -L%pythonPath%libs -lpython36 -L%pythonPath% -lpython36 -o GUI/PyAPI.pyd -shared -std=c++20 -static > BuildOutput.log 2>&1
 echo 编译信息在BuildOutput.log里
